302415d8bd86f1c766c234277a49b5b229453653
tombuildsstuff <git@ibuildstuff.co.uk>
2020-04-27 09:20:23 +0200

    provider-defined-builds: support for multiple environments

--- .teamcity/components/project.kt
+++ .teamcity/components/project.kt
@@ -1,3 +1,4 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 
 var defaultStartHour = 4
@@ -5,7 +6,7 @@ var defaultStartHour = 4
 // this is per-package, not per-concurrent build
 var defaultParallelism = 10
 
-var environment = "public" // TODO: configurable
+var environment = DslContext.getParameter("environment", "public")
 
 object AzureRM : Project({
     vcsRoot(providerRepository)
